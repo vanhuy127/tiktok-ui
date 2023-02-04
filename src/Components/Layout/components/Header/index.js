e9c00ec868e7548939d2376d0fb34f606350397f
huy <huy1272003@yahoo.com>
2023-02-04 17:15:33 +0700

    header#1

--- src/Components/Layout/components/Header/index.js
+++ src/Components/Layout/components/Header/index.js
@@ -1,12 +1,35 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
+
+import images from "~/assets/images";
 
 const cx = classNames.bind(styles);
 
 function Header() {
   return (
     <header className={cx("wrapper")}>
-      <div className={cx("header-content")}></div>
+      <div className={cx("header-content")}>
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="logo" />
+        </div>
+        <div className={cx("search")}>
+          <input placeholder="Search accounts and videos" spellCheck />
+          <button className={cx("clear")}>
+            <FontAwesomeIcon icon={faCircleXmark} />
+          </button>
+          <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+          <button className={cx("search-btn")}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </button>
+        </div>
+        <div className={cx("actions")}></div>
+      </div>
     </header>
   );
 }
